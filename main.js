0a231c685d286257b75af32065d44617b4ef9f21
BasmaMouktaoui <157135703+BasmaMouktaoui@users.noreply.github.com>
2024-02-04 17:23:27 +0100

    .

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
-function toggleNavbar() {
+/*function toggleNavbar() {
     var navList = document.querySelector('.nav-list');
     navList.classList.toggle('show');
-}
+}*/
+
+const cookieBox = document.querySelector(".container"),
+button = document.querySelector(".container");
