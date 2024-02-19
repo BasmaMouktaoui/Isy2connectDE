3177f82cd4e4f01e068fc85cdc24bc56d5e3d7fd
BasmaMouktaoui <157135703+BasmaMouktaoui@users.noreply.github.com>
2024-02-19 11:38:01 +0100

    .

--- main.js
+++ main.js
@@ -3,12 +3,12 @@
     navList.classList.toggle('show');
 }*/
 
-const cookieBox = document.querySelector(".consent-popup"),
+const cookieBox = document.querySelector(".wrapper"),
   buttons = document.querySelectorAll(".button");
 
 const executeCodes = () => {
-  //if cookie contains codinglab it will be returned and below of this code will not run
-  if (document.cookie.includes("isyschool")) return;
+  //if cookie contains Isy2connect it will be returned and below of this code will not run
+  if (document.cookie.includes("isy2connect")) return;
   cookieBox.classList.add("show");
 
   buttons.forEach((button) => {
@@ -18,7 +18,7 @@ const executeCodes = () => {
       //if button has acceptBtn id
       if (button.id == "acceptBtn") {
         //set cookies for 1 month. 60 = 1 min, 60 = 1 hours, 24 = 1 day, 30 = 30 days
-        document.cookie = "cookieBy= isyschool; max-age=" + 60 * 60 * 24 * 30;
+        document.cookie = "cookieBy= isy2connect; max-age=" + 60 * 60 * 24 * 30;
       }
     });
   });
